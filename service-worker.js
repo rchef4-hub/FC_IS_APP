c266bd8c8863230a28fbaf70c2117678a54073f6
rchef4-hub <rchef4@gmail.com>
2026-08-10 16:59:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', (event) => {
   console.log('Service worker installé');
-  // Cache mis à jour à v8 pour forcer la réinstallation
+  // Cache mis à jour à v pour forcer la réinstallation
   event.waitUntil(
-    caches.open('fcis-cache-v8').then(cache => { 
+    caches.open('fcis-cache-v9').then(cache => { 
       return cache.addAll([
         'index.html',
         'app.js',
