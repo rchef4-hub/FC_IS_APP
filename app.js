7cb0bbb0b4248096521736d74230bc7c57daab94
rchef4-hub <rchef4@gmail.com>
2026-08-11 11:44:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <ul class="collapsed">${scorersHTML}</ul>
         <h3 class="accordion-header">👟 Meilleurs Passeurs</h3>
         <ul class="collapsed">${passersHTML}</ul>
-        <h3 class="accordion-header">🟨 Discipline & Cartons</h3>
+        <h3 class="accordion-header">🟨🟥 Discipline</h3>
         <ul class="collapsed">${cardsHTML}</ul>
       `;
 
